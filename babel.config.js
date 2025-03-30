c5badbf7b55671fd798da6a20e08d13c88189fe1
arenazl <arenazl@gmail.com>
2025-03-30 05:43:17 -0300

    OK

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,23 @@
-webpack
-module.exports = {
+
+
+javascriptCopiarmodule.exports = {
   presets: [
     'react-app',
     '@babel/preset-flow'
   ],
   plugins: [
     '@babel/plugin-syntax-flow'
-  ]
+  ],
+  env: {
+    production: {
+      // Configuración específica para producción
+      // Sin React Refresh
+    },
+    development: {
+      // Configuración específica para desarrollo
+      plugins: [
+        // Plugins de desarrollo como React Refresh
+      ]
+    }
+  }
 };
-
-
-ignore: [
-  "**/node_modules/@react-native/**"
-]
